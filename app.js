7440004bbe790f2f9be912ced71c3569a57f61aa
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-06-22 20:27:52 +0530

    database connection setup

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const bodyparser = require("body-parser");
+const path = require("path");
+
+const db = require("./config/database");
+
+// connecting database
+db.authenticate()
+  .then(() => console.log("Database connected successfully!"))
+  .catch((err) => console.log("ERROR connecting database:", err));
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    app: "CodeGig",
+    tutor: "Brad Traversy",
+    description: "Coder's job finding App",
+    motive: "Learning Sequelize ORM with PostgreSQL in NodeJS",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT} 🚀`));
